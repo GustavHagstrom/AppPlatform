cfa5c2f78669689afdbea9a31ce743b03e93011d
Gustav <gustav_.91@hotmail.com>
2023-02-02 17:11:10 +0100

    renaming folders

--- src/BidConExport/DataAccessLibrary/Constants.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace DataAccessLibrary;
-public static class Constants
-{
-    public const string SqlConnectionStringKey = "SQL_CONNECTIONSTRING";
-    public const string MongoConnectopmStringKey = "MONGODB_CONNECTIONSTRING";
-}
