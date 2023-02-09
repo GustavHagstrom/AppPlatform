dc40ceee1d99b09064a151bc2458ec9603b15af8
Gustav <gustav_.91@hotmail.com>
2023-02-09 17:42:50 +0100

    todo fix shared middleware

---
+++ src/BidConReport/Server/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BidConReport.Server.Controllers;
+[Route("[controller]")]
+[ApiController]
+[Authorize]
+public class ValuesController : ControllerBase
+{
+    [HttpGet("get")]
+    public async Task<IActionResult> Get()
+    {
+        var claims = User.Claims.ToList();
+        return await Task.FromResult(Ok(claims));
+    }
+}
---
+++ src/SharedLibrary/Models/Licence.cs
@@ -0,0 +1,12 @@
+﻿using SharedLibrary.Models;
+
+namespace LicenseServer.Models;
+
+public class Licence
+{
+    public Guid Id { get; set; }
+    public required string OrganizationId { get; set; }
+    public required string Name { get; set; }
+    public required DateTime ExpireDate { get; set; }
+    public required Application Application { get;set; }
+}
