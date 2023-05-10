644d2f1820d3a81b3a98bfe1e234beac08511013
Gustav <gustav_.91@hotmail.com>
2023-05-10 17:04:16 +0200

    Collie progress

---
+++ src/Collie/Client/CustomThemes.cs
@@ -0,0 +1,42 @@
+﻿using MudBlazor;
+
+namespace Collie.Client;
+
+public static class CustomThemes
+{
+    public static MudTheme CollieStandard => new MudTheme
+    {
+        Palette = new PaletteLight
+        {
+            Primary = Colors.Blue.Darken1,
+            PrimaryLighten = Colors.Blue.Lighten1,
+            PrimaryDarken = Colors.Blue.Darken2,
+        },
+        PaletteDark = new PaletteDark
+        {
+
+        },
+        LayoutProperties = new LayoutProperties
+        {
+            DefaultBorderRadius = "0",
+            
+        },
+        PseudoCss = new PseudoCss
+        {
+
+        },
+        Shadows = new Shadow
+        {
+            
+        },
+        Typography = new Typography
+        {
+            Button = new Button { TextTransform = "none" },
+        },
+        ZIndex = new ZIndex
+        {
+
+        },
+    };
+
+}
--- src/Collie/Client/Program.cs
+++ src/Collie/Client/Program.cs
@@ -2,6 +2,7 @@ using Collie.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -12,6 +13,8 @@ builder.Services.AddHttpClient("Collie.ServerAPI", client => client.BaseAddress 
 
 // Supply HttpClient instances that include access tokens when making requests to the server project
 builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>().CreateClient("Collie.ServerAPI"));
+builder.Services.AddMudServices();
+builder.Services.AddLocalization();
 
 builder.Services.AddMsalAuthentication(options =>
 {
