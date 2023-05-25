a6d47874576d86cc4579382dc686ae89c3020adf
Gustav <gustav_.91@hotmail.com>
2023-05-25 17:10:36 +0200

    appplatform sln

--- src/Collie/Tests/TempTests.cs
+++ src/Collie/Tests/TempTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,20 +9,15 @@ namespace Collie.Tests;
 public class TempTests
 {
     //[Test]
-    [TestCase("mypage/lol")]
-    [TestCase("/mypage/lol")]
-    [TestCase("")]
-    public void TempTest1(string href)
+    [TestCase("VX3EEWKNQrrBQp+52Ct5Gw==")]
+    [TestCase("1xIFE/OYDJuUDSGwDNJUTw==")]
+    public void TempTest1(string encryptedPassword)
     {
-        //string uri = "http://www.google.se/mypage/lol/1/";
-        //var actualPath = uri.Replace("http://www.google.se/", string.Empty);
-
-        //if (href.FirstOrDefault() == '/')
-        //{
-        //    href = href[1..];
-        //}
-        
-
-        Assert.That(href[0..href.Length], Is.EqualTo(href));
+        byte[] eNCRYPTION_KEY = new byte[8] { 45, 103, 246, 79, 36, 99, 167, 3 };
+        byte[] rgbIV = new byte[8] { 55, 103, 246, 79, 36, 99, 167, 3 };
+        DESCryptoServiceProvider dESCryptoServiceProvider = new();
+        byte[] array = Convert.FromBase64String(encryptedPassword);
+        var decryptedPass = new StreamReader(new CryptoStream(new MemoryStream(array), dESCryptoServiceProvider.CreateDecryptor(eNCRYPTION_KEY, rgbIV), CryptoStreamMode.Read)).ReadToEnd();
+        Assert.That(decryptedPass, Is.EqualTo("lol"));
     }
 }
