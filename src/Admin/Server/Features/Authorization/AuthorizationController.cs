a66cc164e24d5828fb24736d9763744da14c26dc
Gustav <gustav_.91@hotmail.com>
2023-06-01 21:38:17 +0200

    auth now works across all apps

---
+++ src/Admin/Server/Features/Authorization/AuthorizationController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Server.Features.Authorization;
+[Route("api/[controller]")]
+[ApiController]
+public class AuthorizationController : ControllerBase
+{
+
+    [HttpGet("Claims")]
+    public IActionResult GetClaims(CancellationToken cancellationToken)
+    {
+        var claims = new List<KeyValuePair<string, string>>
+        {
+            new KeyValuePair<string, string>("ClaimType1", "Value1"),
+            new KeyValuePair<string, string>("ClaimType2", "Value2")
+        };
+        return Ok(claims);
+    }
+}
