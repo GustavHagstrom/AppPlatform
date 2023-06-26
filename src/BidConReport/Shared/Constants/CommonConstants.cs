8f358b40e4a5ebb4dd1b3da6d97c6743ba6e2c82
Gustav <gustav_.91@hotmail.com>
2023-06-26 15:19:16 +0200

    role constants

--- src/BidConReport/Shared/Constants/CommonConstants.cs
+++ src/BidConReport/Shared/Constants/CommonConstants.cs
@@ -9,5 +9,21 @@ public static class CommonAppConstants
     /// <summary>
     /// This is used in the licenseDb and probably a bad idea to change once in production
     /// </summary>
-    public static readonly string[] AppRoles = { "admin", "creator", "reader", "guest" };
+    public static readonly string[] AppRoles = 
+    { 
+
+        RoleConstants.Admin,
+        RoleConstants.Creator,
+        RoleConstants.Reader,
+        RoleConstants.Guest,
+    };
+
+    public static class RoleConstants
+    {
+        public const string Admin = "admin";
+        public const string Creator = "creator";
+        public const string Reader = "reader";
+        public const string Guest = "guest";
+    }
+    
 }
