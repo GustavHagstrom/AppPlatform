e3bf000c7cc6c7d24c75e4823ef44530d9a75657
Gustav <gustav_.91@hotmail.com>
2023-06-30 02:14:55 +0200

    Size change event now works

--- src/Shared Libraries/SharedWasmLibrary/Shared/Services/ScreenSizeService.cs
+++ src/Shared Libraries/SharedWasmLibrary/Shared/Services/ScreenSizeService.cs
@@ -4,29 +4,38 @@ namespace SharedWasmLibrary.Shared.Services;
 public class ScreenSizeService
 {
     private readonly IJSRuntime _jSRuntime;
-    private bool _isInitialized;
+    private bool _isInitialized = false;
 
     public ScreenSizeService(IJSRuntime jSRuntime)
     {
         _jSRuntime = jSRuntime;
     }
-    public async Task InitializeAsync()
+    public async Task Subscribe(Action<int> triggerAction)
+    {
+        await InitializeAsync();
+        ScreenWidthChanged += triggerAction;
+    }
+    public void UnSubscribe(Action<int> triggerAction)
+    {
+        ScreenWidthChanged -= triggerAction;
+    }
+    private async Task InitializeAsync()
     {
         if (_isInitialized) return;
 
         _isInitialized = true;
         var dotnetRef = DotNetObjectReference.Create(this);
-        await _jSRuntime.InvokeVoidAsync("addEventListener", "resize", dotnetRef, "OnBrowserResize");
+        await _jSRuntime.InvokeVoidAsync("window.resizeInterop.registerResizeCallback", dotnetRef, "OnWindowResize");
         CurrentScreenWidth = await GetWidnowWidth();
     }
 
     public int CurrentScreenWidth { get; set; }
-    public event EventHandler<int>? ScreenWidthChanged;
+    public event Action<int>? ScreenWidthChanged;
     [JSInvokable]
-    private void OnWindowResize()
+    public void OnWindowResize()
     {
-        CurrentScreenWidth = GetWidnowWidth().Result;
-        ScreenWidthChanged?.Invoke(this, CurrentScreenWidth);
+        ScreenWidthChanged?.Invoke(CurrentScreenWidth);
+        _ = Task.Run(async () => CurrentScreenWidth = await GetWidnowWidth());
     }
     private async Task<int> GetWidnowWidth()
     {
