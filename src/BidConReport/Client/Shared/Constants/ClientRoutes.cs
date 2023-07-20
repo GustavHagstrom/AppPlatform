0582561546bc61a125bba0cbb75f3ddfc5c21027
Gustav <gustav_.91@hotmail.com>
2023-07-20 18:21:43 +0200

    Changed client Route

--- src/BidConReport/Client/Shared/Constants/ClientRoutes.cs
+++ src/BidConReport/Client/Shared/Constants/ClientRoutes.cs
@@ -4,7 +4,7 @@ public static class ClientRoutes
 {
     public const string Dashboard = "/dashboard";
     public const string Import = "/import";
-    public const string Post = "/publish";
+    public const string Report = "/report";
     public const string Settings = "/settings";
     /// <summary>
     /// This class is empty. The authentication routes are defined in the SharedWasmLibrary
