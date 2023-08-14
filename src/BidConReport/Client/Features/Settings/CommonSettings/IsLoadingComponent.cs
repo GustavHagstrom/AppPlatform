5be4778df7a42fbd673925e2182d2376c06c597d
Gustav <gustav_.91@hotmail.com>
2023-08-14 12:31:05 +0200

    IsLoadingComponent

---
+++ src/BidConReport/Client/Features/Settings/CommonSettings/IsLoadingComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BidConReport.Client.Features.Settings.CommonSettings;
+
+public class IsLoadingComponent : ComponentBase
+{
+    [Parameter] 
+    public EventCallback<bool> IsLoadingChanged { get; set; }
+    public bool IsLoading { get; private set; } = true;
+    public async Task SetLoading(bool isLoading)
+    {
+        IsLoading = isLoading;
+        await IsLoadingChanged.InvokeAsync(isLoading);
+        StateHasChanged();
+    }
+}
