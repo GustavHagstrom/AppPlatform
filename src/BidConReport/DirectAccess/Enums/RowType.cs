089e419ef55f75fcdfd71e2f20a29a24390c1724
Gustav <gustav_.91@hotmail.com>
2023-08-24 14:11:18 +0200

    types

--- src/BidConReport/DirectAccess/Enums/RowType.cs
+++ src/BidConReport/DirectAccess/Enums/RowType.cs
@@ -1,10 +1,12 @@
 ﻿namespace BidConReport.BidconDatabaseAccess.Enums;
 public enum RowType
 {
+    None = 0,
     Group = 100,
     Part = 101,
     LayeredItem = 102,
     QuantityItem = 106,
     LockedStageCollection = 107,
     Text = 108,
+    Invoice = 109,
 }
--- src/BidConReport/DirectAccess/Enums/SheetTypes.cs
+++ src/BidConReport/DirectAccess/Enums/SheetTypes.cs
@@ -1,7 +1,11 @@
 ﻿namespace BidConReport.BidconDatabaseAccess.Enums;
 public enum SheetTypes
 {
+    None = 0,
     NetSheet = 1,
+    TenderSheet = 2,
+    ExpenseSheet = 4,
+    ResourceSheet = 8,
     SummarySheet = 16,
     LockedStageSheet = 64,
 }
