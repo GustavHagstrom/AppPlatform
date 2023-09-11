796b1702d2d02e8ef1067c5222fb04796a8047ed
Gustav <gustav_.91@hotmail.com>
2023-09-11 16:58:11 +0200

    Removed unused enteties

--- src/BidConReport/Server/Data/ApplicationDbContext.cs
+++ src/BidConReport/Server/Data/ApplicationDbContext.cs
@@ -1,8 +1,6 @@
 ﻿using BidConReport.Server.Enteties;
-using BidConReport.Server.Enteties.Report;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
-using System.Text.Json;
 
 namespace BidConReport.Server.Data;
 
@@ -16,22 +14,15 @@ public class ApplicationDbContext : DbContext
 
     }
 
-    public DbSet<EstimationImportSettings> EstimationImportSettings { get; set; }
-    public DbSet<Estimation> Estimations { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Organization> Organizations { get; set; }
     public DbSet<BidconCredentials> BidconCredentials { get; set; }
-    //public DbSet<UserOrganization> UserOrganizations { get; set; }
-    public DbSet<ReportTemplate> ReportTemplates { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
 
         MapUserAndOrganizations(modelBuilder);
-        MapEstimation(modelBuilder);
-        MapImportSettings(modelBuilder);
-        MapReportTemplate(modelBuilder);
 
     }
     private void MapUserAndOrganizations(ModelBuilder modelBuilder)
@@ -41,111 +32,4 @@ public class ApplicationDbContext : DbContext
             .WithOne(u => u.Organization)
             .HasForeignKey(u => u.OrganizationId);
     }
-    private void MapEstimation(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Estimation>()
-            .Property(e => e.QuickTags)
-            .HasConversion(
-            v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),
-            v => (JsonSerializer.Deserialize<ICollection<string>>(v, new JsonSerializerOptions())!))
-            .HasColumnType("NVARCHAR(1000)")
-            .Metadata.SetValueComparer(_stringCollectionValueComparer);
-        modelBuilder.Entity<Estimation>()
-            .Property(e => e.SelectionTags)
-            .HasConversion(
-            v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),
-            v => (JsonSerializer.Deserialize<ICollection<string>>(v, new JsonSerializerOptions())!))
-            .HasColumnType("NVARCHAR(1000)")
-            .Metadata.SetValueComparer(_stringCollectionValueComparer);
-
-        modelBuilder.Entity<EstimationItem>()
-            .Property(e => e.Tags)
-            .HasConversion(
-            v => JsonSerializer.Serialize(v, new JsonSerializerOptions()),
-            v => (JsonSerializer.Deserialize<ICollection<string>>(v, new JsonSerializerOptions())!))
-            .HasColumnType("NVARCHAR(1000)")
-            .Metadata.SetValueComparer(_stringCollectionValueComparer);
-    }
-    private void MapImportSettings(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<EstimationImportSettings>()
-           .Property(e => e.QuickTags)
-           .HasConversion(
-               v => string.Join(",", v),
-               v => v.Split(",", StringSplitOptions.RemoveEmptyEntries).ToArray())
-           .HasColumnType("NVARCHAR(1000)")
-           .Metadata.SetValueComparer(_stringCollectionValueComparer);
-        modelBuilder.Entity<EstimationImportSettings>()
-            .Property(e => e.SelectionTags)
-            .HasConversion(
-                v => string.Join(",", v),
-                v => v.Split(",", StringSplitOptions.RemoveEmptyEntries).ToArray())
-            .HasColumnType("NVARCHAR(1000)")
-            .Metadata.SetValueComparer(_stringCollectionValueComparer);
-    }
-    private void MapReportTemplate(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<InformationSection>()
-            .WithOne(x => x.ValueFont)
-            .HasForeignKey(x => x.ValueFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<InformationSection>()
-            .WithOne(x => x.TitleFont)
-            .HasForeignKey(x => x.TitleFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<HeaderDefinition>()
-            .WithOne(x => x.Font)
-            .HasForeignKey(x => x.FontId)
-            .OnDelete(DeleteBehavior.NoAction);
-
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<TableSection>()
-            .WithOne(x => x.CellFont)
-            .HasForeignKey(x => x.CellFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<TableSection>()
-            .WithOne(x => x.PartFont)
-            .HasForeignKey(x => x.PartFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<TableSection>()
-            .WithOne(x => x.GroupFont)
-            .HasForeignKey(x => x.GroupFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<TableSection>()
-            .WithOne(x => x.ColumnHeaderFont)
-            .HasForeignKey(x => x.ColumnHeaderFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<PriceSection>()
-            .WithOne(x => x.CommentFont)
-            .HasForeignKey(x => x.CommentFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-        modelBuilder.Entity<FontProperties>()
-            .HasMany<PriceSection>()
-            .WithOne(x => x.PriceFont)
-            .HasForeignKey(x => x.PriceFontId)
-            .OnDelete(DeleteBehavior.NoAction);
-
-        modelBuilder.Entity<HeaderDefinition>()
-            .HasMany<ReportTemplate>()
-            .WithOne(x => x.TopLeftHeader)
-            .HasForeignKey(x => x.TopLeftHeaderId)
-            .OnDelete(DeleteBehavior.NoAction);
-        modelBuilder.Entity<HeaderDefinition>()
-            .HasMany<ReportTemplate>()
-            .WithOne(x => x.TopRightHeader)
-            .HasForeignKey(x => x.TopRightHeaderId)
-            .OnDelete(DeleteBehavior.NoAction);
-
-    }
-
-
 }
--- src/BidConReport/Server/Enteties/Report/IReportTemplateSection.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BidConReport.Server.Enteties.Report;
-public interface IReportTemplateSection
-{
-    int LayoutOrder { get; set; }
-    bool IsEnabled { get; set; }
-}
