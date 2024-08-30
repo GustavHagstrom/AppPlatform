a66830501b057c8a9a505a51a88c7143ac624d45
Gustav <gustav_.91@hotmail.com>
2024-08-30 12:53:30 +0200

    Refactor table components for hierarchical data support
    Refactored `SheetItemRow` and `SheetSectionPresenter` to use the new `SheetItemContainer` class for managing hierarchical data and item expansion states. Introduced `SheetItemCell` component for individual cell rendering. Updated parameters and methods to support these changes. Adjusted CSS styles for new components and features.

---
+++ src/AppPlatform.Shared/Components/View/SheetItemContainer.cs
@@ -0,0 +1,50 @@
+﻿using AppPlatform.Core.Enteties.EstimationEnteties;
+using Microsoft.Graph.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace AppPlatform.Shared.Components.View;
+public class SheetItemContainer
+{
+    public SheetItemContainer(SheetItem sheetItem, bool isExpanded = true, SheetItemContainer? parent = null)
+    {
+        SheetItem = sheetItem;
+        IsExpanded = isExpanded;
+        Parent = parent;
+        Children.AddRange(SheetItem.Children.Select(x => new SheetItemContainer(x, isExpanded, this)));
+    }
+
+    public SheetItem SheetItem { get; }
+    public SheetItemContainer? Parent { get; set; }
+    public List<SheetItemContainer> Children { get; set; } = new();
+    public bool IsExpanded { get; set; }
+    public int Level
+    {
+        get
+        {
+            if (Parent is null)
+            {
+                return 0;
+            }
+            return Parent.Level + 1;
+        }
+    }
+    public IEnumerable<SheetItemContainer> AllExpandedInOrder
+    {
+        get
+        {
+            //If this is expanded return this and all children
+            if (IsExpanded)
+            {
+                return new[] { this }.Concat(Children.SelectMany(x => x.AllExpandedInOrder));
+            }
+            //If this is not expanded return this
+            return new[] { this };
+
+        }
+    }
+    public override string ToString()
+    {
+        return SheetItem.Description;
+    }
+}
