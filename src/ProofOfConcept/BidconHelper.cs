baa2f220e24f87281891f31ae6c624ba25dd6a1b
Gustav <gustav_.91@hotmail.com>
2024-11-25 15:28:15 +0100

    BidCon without license proof of concept

---
+++ src/ProofOfConcept/BidconHelper.cs
@@ -0,0 +1,69 @@
+﻿using BidCon.SDK.Database;
+using System.Data.SqlClient;
+using System.Runtime.CompilerServices;
+
+namespace ConsoleApp1;
+internal static class BidconHelper
+{
+    public static DatabaseUser CreateUser()
+    {
+        var connection = ConnectionFromIni();
+        var usr = (DatabaseUser)RuntimeHelpers.GetUninitializedObject(typeof(DatabaseUser));
+
+        ReflectionHelper.SetFieldValue(usr, "<Name>k__BackingField", "Admin");
+        ReflectionHelper.SetFieldValue(usr, "<ID>k__BackingField", 2);
+        ReflectionHelper.SetFieldValue(usr, "<UserGroup>k__BackingField", 2);
+        
+        ReflectionHelper.SetFieldValue(usr, "<EstimationAccess>k__BackingField", CreateEstimationAccess(connection, usr));
+        ReflectionHelper.SetFieldValue(usr, "_estimationReader", CreateEstimationReader(connection, usr));
+        ReflectionHelper.SetFieldValue(usr, "_systemReader", CreateSystemReader(connection));
+
+        // Define key and value types dynamically
+        Type keyType = typeof(string);
+        Type valueType = Type.GetType("BidCon.SDK.Database.DatabaseEstimationWriter, BidCon.SDK, Version=2023.2.0.0, Culture=neutral, PublicKeyToken=d5af2ed775e84a93")!;
+
+        // Create a generic Dictionary<TKey, TValue> type
+        Type dictionaryType = typeof(Dictionary<,>).MakeGenericType(keyType, valueType);
+
+        // Instantiate the dictionary
+        object dictionary = System.Activator.CreateInstance(dictionaryType)!;
+
+        ReflectionHelper.SetFieldValue(usr, "EstimationWriters", dictionary);
+        ReflectionHelper.SetFieldValue(usr, "<Connection>k__BackingField", connection);
+
+
+
+        return usr;
+    }
+    public static object CreateEstimationAccess(object connection, DatabaseUser user)
+    {
+        var accessType = Type.GetType("BidCon.SDK.Database.DatabaseEstimationAccess, BidCon.SDK, Version=2023.2.0.0, Culture=neutral, PublicKeyToken=d5af2ed775e84a93");
+        var instance = ReflectionHelper.CreateInstance(accessType!, [user, connection]);
+        return instance;
+    }
+    public static DatabaseEstimationReader CreateEstimationReader(object connection, DatabaseUser user)
+    {
+        var reader = ReflectionHelper.CreateInstance<DatabaseEstimationReader>(user, connection);
+        return reader;
+    }
+    public static DatabaseSystemReader CreateSystemReader(object connection)
+    {
+        var reader = ReflectionHelper.CreateInstance<DatabaseSystemReader>(connection);
+        return reader;
+    }
+    public static object ConnectionFromIni()
+    {
+        var app = BidCon.SDK.Activator.CreateApp();
+        app.InitConnectionFromIni();
+        return ReflectionHelper.GetFieldOrPropertyValue<object>(app, "_connection");
+    }
+    public static SqlConnection GetSystemConnection()
+    {
+        return new SqlConnection("Data Source=RHUSAPP02\\ELECOSOFT;Initial Catalog=BidConSystem;User ID=sa;Password=Putlig@15;Connect Timeout=30;Encrypt=False;");
+    }
+    public static SqlConnection GetEstimationConnection()
+    {
+        return new SqlConnection("Data Source=RHUSAPP02\\ELECOSOFT;Initial Catalog=BidConEstimation;User ID=sa;Password=Putlig@15;Connect Timeout=30;Encrypt=False;");
+    }
+
+}
---
+++ src/ProofOfConcept/ReflectionHelper.cs
@@ -0,0 +1,192 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace ConsoleApp1;
+internal static class ReflectionHelper
+{
+    /// <summary>
+    /// Creates an instance of a class with an internal, private, or protected constructor.
+    /// </summary>
+    /// <typeparam name="T">The type of object to create.</typeparam>
+    /// <param name="constructorArgs">Arguments for the constructor.</param>
+    /// <returns>An instance of the specified type.</returns>
+    public static T CreateInstance<T>(params object[] constructorArgs)
+    {
+        var type = typeof(T);
+        return (T)CreateInstance(type, constructorArgs);
+    }
+    public static object CreateInstance(Type type, params object[] constructorArgs)
+    {
+        var constructor = type.GetConstructor(
+            BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+            null,
+            Array.ConvertAll(constructorArgs, arg => arg.GetType()),
+            null);
+
+        if (constructor == null)
+        {
+            throw new MissingMethodException($"No matching constructor found for type {type.FullName}.");
+        }
+
+        return constructor.Invoke(constructorArgs);
+    }
+
+
+    /// <summary>
+    /// Gets the value of a private, internal, or protected field or property.
+    /// </summary>
+    /// <typeparam name="T">The type of the value to retrieve.</typeparam>
+    /// <param name="instance">The object instance containing the field or property.</param>
+    /// <param name="memberName">The name of the field or property.</param>
+    /// <returns>The value of the field or property.</returns>
+    public static T GetFieldOrPropertyValue<T>(object instance, string memberName)
+    {
+        var type = instance.GetType();
+
+        // Try to get the field
+        var field = type.GetField(memberName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        if (field != null)
+        {
+            return (T)field.GetValue(instance);
+        }
+
+        // Try to get the property
+        var property = type.GetProperty(memberName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+        if (property != null)
+        {
+            return (T)property.GetValue(instance);
+        }
+
+        throw new MissingMemberException($"No field or property named '{memberName}' found on type {type.FullName}.");
+    }
+    /// <summary>
+    /// Sets the value of a private, internal, or protected field.
+    /// </summary>
+    /// <param name="instance">The object instance containing the field.</param>
+    /// <param name="fieldName">The name of the field.</param>
+    /// <param name="value">The new value for the field.</param>
+    public static void SetFieldValue(object instance, string fieldName, object value)
+    {
+        var type = instance.GetType();
+        var field = type.GetField(fieldName, BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+        if (field == null)
+        {
+            throw new MissingFieldException($"No field named '{fieldName}' found on type {type.FullName}.");
+        }
+
+        field.SetValue(instance, value);
+    }
+
+    /// <summary>
+    /// Invokes a private, internal, or protected method on an object instance.
+    /// </summary>
+    /// <typeparam name="T">The return type of the method.</typeparam>
+    /// <param name="instance">The object instance containing the method.</param>
+    /// <param name="methodName">The name of the method to invoke.</param>
+    /// <param name="methodArgs">Arguments for the method.</param>
+    /// <returns>The result of the method invocation.</returns>
+    public static T InvokeMethod<T>(object instance, string methodName, params object[] methodArgs)
+    {
+        var type = instance.GetType();
+        var method = type.GetMethod(
+            methodName,
+            BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+        if (method == null)
+        {
+            throw new MissingMethodException($"No method named '{methodName}' found on type {type.FullName}.");
+        }
+
+        return (T)method.Invoke(instance, methodArgs);
+    }
+    public static void InvokeMethod(object instance, string methodName, params object[] methodArgs)
+    {
+        var type = instance.GetType();
+        var method = type.GetMethod(
+            methodName,
+            BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+        if (method == null)
+        {
+            throw new MissingMethodException($"No method named '{methodName}' found on type {type.FullName}.");
+        }
+
+        method.Invoke(instance, methodArgs);
+    }
+    /// <summary>
+    /// Invokes a private, internal, or protected static method on a specified class.
+    /// </summary>
+    /// <typeparam name="TClass">The type of the class containing the static method.</typeparam>
+    /// <typeparam name="TReturn">The expected return type of the method.</typeparam>
+    /// <param name="methodName">The name of the static method to invoke.</param>
+    /// <param name="methodArgs">Arguments for the static method.</param>
+    /// <returns>The result of the method invocation, or default(TReturn) if the method returns void.</returns>
+    public static TReturn? InvokeStaticMethod<TClass, TReturn>(string methodName, params object[] methodArgs)
+    {
+        Type type = typeof(TClass);
+
+        // Look for the method in the specified class
+        var method = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+        if (method == null)
+        {
+            throw new MissingMethodException($"The static method '{methodName}' was not found in type '{type.FullName}'.");
+        }
+
+        // Invoke the method
+        var result = method.Invoke(null, methodArgs);
+
+        // Return the result, or default(TReturn) if the method is void
+        return (TReturn?)result;
+    }
+    /// <summary>
+    /// Invokes a private, internal, or protected static method on a specified class.
+    /// </summary>
+    /// <typeparam name="TClass">The type of the class containing the static method.</typeparam>
+    /// <typeparam name="TReturn">The expected return type of the method.</typeparam>
+    /// <param name="methodName">The name of the static method to invoke.</param>
+    /// <param name="methodArgs">Arguments for the static method.</param>
+    /// <returns>The result of the method invocation, or default(TReturn) if the method returns void.</returns>
+    public static void InvokeStaticMethod<T>(string methodName, params object[] methodArgs)
+    {
+        Type type = typeof(T);
+
+        // Look for the method in the specified class
+        var method = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+        if (method == null)
+        {
+            throw new MissingMethodException($"The static method '{methodName}' was not found in type '{type.FullName}'.");
+        }
+
+        // Invoke the method
+        method.Invoke(null, methodArgs);
+    }
+    /// <summary>
+    /// Creates a subclass of an internal class with a specified name.
+    /// </summary>
+    /// <param name="internalBaseType"></param>
+    /// <param name="newClassName"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static Type CreateSubclass(Type internalBaseType, string newClassName)
+    {
+        if (!internalBaseType.IsClass || internalBaseType.IsNotPublic)
+        {
+            throw new ArgumentException("The provided type must be an internal class.", nameof(internalBaseType));
+        }
+
+        // Define a dynamic assembly and module
+        var assemblyName = new AssemblyName("DynamicAssembly");
+        var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+        var moduleBuilder = assemblyBuilder.DefineDynamicModule("DynamicModule");
+
+        // Define a new public class that extends the internal base class
+        var typeBuilder = moduleBuilder.DefineType(
+            newClassName,
+            TypeAttributes.Public | TypeAttributes.Class,
+            internalBaseType);
+
+        // Create the type
+        return typeBuilder.CreateType();
+    }
+}
