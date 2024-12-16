c4a148b5a51c717f17f5198332dc26bbf4f9970a
Gustav <gustav_.91@hotmail.com>
2024-12-16 22:23:33 +0100

    Add check for existing MongoDB collection before creation
    Ensure MongoDB collection is created only if it doesn't exist.
    This prevents potential errors or redundant operations when
    the collection is already present in the database.

--- src/Core/Builders/MongoCollectionRegistrar.cs
+++ src/Core/Builders/MongoCollectionRegistrar.cs
@@ -16,6 +16,10 @@ public class MongoCollectionRegistrar(IServiceCollection services)
         services.AddSingleton(sp =>
         {
             var db = sp.GetRequiredService<IMongoDatabase>();
+            if(!db.ListCollectionNames().ToList().Contains(collectionName))
+            {
+                db.CreateCollection(collectionName);
+            }
             return db.GetCollection<T>(collectionName);
         });
     }
